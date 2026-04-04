d2a04afeb28cb091a330c4cb956e2f8f83127ede
arianjackobcorreia <sophyjackobcorreia539@gmail.com>
2026-04-04 10:59:23 -0300

    Atualização das cartas
    Formatei as cartas para receber cores, e mais alguns ajustes

---
+++ util/Cores.java
@@ -0,0 +1,14 @@
+package util;
+
+public class Cores {
+    public static final String RESET = "\u001B[0m";
+
+    public static final String AZUL = "\u001B[34m";
+    public static final String VERDE = "\u001B[32m";
+    public static final String AMARELO = "\u001B[33m";
+    public static final String VERMELHO = "\u001B[31m";
+    public static final String ROXO = "\u001B[35m";
+    public static final String CIANO = "\u001B[36m";
+
+    public static final String NEGRITO = "\u001B[1m";
+}
